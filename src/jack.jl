319f5634961f88e93d7935b324cd2cc08b7c3f0a
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-10-21 15:28:24 -0400

    Add `eval_jack`

--- src/jack.jl
+++ src/jack.jl
@@ -31,3 +31,41 @@ function egen(κ::AbstractVector)
     end
     lst2
 end
+
+# TODO: optimize
+function eval_jack(a, l, x)
+    if length(x) < length(l)
+        return 0
+    elseif isempty(l)
+        return 1
+    elseif length(x) == 1
+        return x[1]^l[1] * a^(l[1] - 1) * sfact(1 / a + 1, l[1] - 1)
+    end
+    ls = sum(l)
+    lc = conjugate(l)
+    uvalid = egen(lc)
+    res = 0
+    for u in uvalid
+        uc = u
+        u = conjugate(u)
+        us = sum(u)
+        ab = 1
+        for i in eachindex(u), j in 1:u[i]
+            if j ≤ length(uc) && j ≤ length(lc) && lc[j] == uc[j]
+                ab /= uc[j] - i + a * (u[i] - j + 1)
+            else
+                ab /= uc[j] - i + 1 + a * (u[i] - j)
+            end
+        end
+        for i in eachindex(l), j in 1:l[i]
+            if j ≤ length(uc) && j ≤ length(lc) && lc[j] == uc[j]
+                ab *= lc[j] - i + a * (l[i] - j + 1)
+            else
+                ab *= lc[j] - i + 1 + a * (l[i] - j)
+            end
+        end
+        term = x[end]^(ls - us) * ab * eval_jack(a, u, @view x[1:(end - 1)])
+        res += term
+    end
+    res
+end
--- test/jack.jl
+++ test/jack.jl
@@ -9,3 +9,14 @@ using Test, MOPS
     @test MOPS.egen([3, 3, 2]) ==
           [[2, 2, 1], [2, 2, 2], [3, 2, 1], [3, 2, 2], [3, 3, 1], [3, 3, 2]]
 end
+
+@testset "eval_jack" begin
+    @test MOPS.eval_jack(1, [4, 2, 1], [5, 3, 2]) == 7944480
+    @test MOPS.eval_jack(2, [4, 2, 1], [5, 3, 2]) == 28874880
+    @test MOPS.eval_jack(1, [4, 2, 2, 1], [5, 3, 2]) == 0
+    @test MOPS.eval_jack(1, [4, 2, 2, 1], [5, 3, 2, 1]) == 229320000
+    @test MOPS.eval_jack(2, [4, 2, 2, 1], [5, 3, 2, 1]) ≈ 924636000
+    @test MOPS.eval_jack(1, [4], [5]) == 15000
+    @test MOPS.eval_jack(2, [4], [5]) == 65625
+    @test MOPS.eval_jack(1, [], [5]) == 1
+end
